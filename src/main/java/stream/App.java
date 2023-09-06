ba5626affcaf3b424865928d9958f9278fb9f2fe
Taixing Bi <taixingbi@taixings-mbp.lan>
2023-09-06 14:00:52 -0400

    add more

--- src/main/java/stream/App.java
+++ src/main/java/stream/App.java
@@ -6,16 +6,22 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
         System.out.println("---------------");
-        Map<String, String> map =  new HashMap<>();
+        Map<String, String> map = new HashMap<>();
 
-        Optional optional = Optional.empty();;
-        optional = Optional.of(map);
+        Optional optional = Optional.empty();
+        ;
+//        optional = Optional.of(map);
 
-        if (optional.isEmpty()){
+        if (optional.isEmpty()) {
             System.out.println("isEmpty");
         } else {
             System.out.println("not isEmpty");
+        }
 
+        if (optional.isPresent()) {
+            System.out.println("isPresent");
+        } else {
+            System.out.println("not isPresent");
         }
     }
 }
