c3de1e0a0b4011a80de4addaa0dcd2a698b20120
Taixing Bi <taixingbi@taixings-mbp.lan>
2023-09-06 10:31:23 -0400

    first commit

---
+++ src/main/java/stream/App.java
@@ -0,0 +1,21 @@
+package stream;
+
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("---------------");
+        Map<String, String> map =  new HashMap<>();
+
+        Optional optional = Optional.empty();;
+        optional = Optional.of(map);
+
+        if (optional.isEmpty()){
+            System.out.println("isEmpty");
+        } else {
+            System.out.println("not isEmpty");
+
+        }
+    }
+}
